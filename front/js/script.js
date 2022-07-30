796b7cfb5367cef39fc5e160962ddb1cfc8cc978
Goro-K <69romankizil69@gmail.com>
2022-07-30 20:06:18 +0200

    ajout des commentaires sur la page produit

--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,18 @@
 async function listProducts() {
+    // Récuperation de la liste des produits avec la méthode fetch
     const response = await fetch("http://localhost:3000/api/products")
     const products = await response.json()
     return products
 }
 
+// stocker le résultat de la promesse = un tableau de produit
 
 const resultat = await listProducts()
 
 
-
+// Pour parcourir les donnée de resultat on crée une boucle
 resultat.forEach(product => {
+    // on utilise innerHTML += ' ' pour ne pas perdre l'itération précédente
     document.querySelector(".items").innerHTML +=
     `<a href= "${product._id}"> 
     <article>
