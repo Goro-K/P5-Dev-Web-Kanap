1c2ef1b29628f9fb6b4b960b3ce60db0e486ea13
Goro-K <69romankizil69@gmail.com>
2022-08-02 23:08:51 +0200

    Site terminée

---
+++ front/js/product.js
@@ -0,0 +1,11 @@
+let params = new URL(window.location).searchParams;
+let id = params.get("id");
+console.log(id)
+
+
+async function listId() {
+    // Récuperation de la liste des produits avec la méthode fetch
+    const response = await fetch("http://localhost:3000/api/products/_id")
+    const products = await response.json()
+    return products
+}
